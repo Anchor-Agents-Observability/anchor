9b122481b99d66a5a1021546014699d383c09689
Hlaing, Khant Nyi <knyihlaing@go.pasadena.edu>
2025-12-18 14:27:37 -0800

    semantic conventions

--- src/anchor-sdk-python/anchor/instrument_mapper.py
+++ src/anchor-sdk-python/anchor/instrument_mapper.py
@@ -7,3 +7,20 @@ MODULE_MAP = {
 INSTRUMENT_MAP = {
     "openai": "anchor.instrumentation.openai.openaiInstrumentor",
 }
+
+def get_instrumentor(name):
+    """
+    Get the instrumentor collections for the given module.
+    """
+    if name not in INSTRUMENT_MAP:
+        raise ValueError(f"Instrumentor {name} not found.")
+    
+    path, instrName = INSTRUMENT_MAP[name].rsplit('.', 1)
+
+    try:
+        module = importlib.import_module(path)
+        # returns instance of the instrumentor class
+        return getattr(module, instrName)
+    except ImportError:
+        raise ImportError(f"Instrumentor {name} not found.")
+
--- src/anchor-sdk-python/anchor/instrumentation/openai/__init__.py
+++ src/anchor-sdk-python/anchor/instrumentation/openai/__init__.py
@@ -2,5 +2,31 @@
 initialize auto-instrumentation for open ai 
 """
 
-class openaiInstrumentor(Base):
+from typing import Collection
+from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
+
+from anchor.instrumentation.openai.openai import ()
+
+from anchor.instrumentation.openai.openai import (
+    chat_completions,
+    embedding,
+    responses,
+    chat_completions_parse,
+    image_generate,
+    image_variatons,
+    audio_create,
+)
+from anchor.instrumentation.openai.async_openai import (
+    async_chat_completions,
+    async_embedding,
+    async_chat_completions_parse,
+    async_image_generate,
+    async_image_variations,
+    async_audio_create,
+    async_responses,
+)
+
+class openaiInstrumentor(BaseInstrumentor):
+
+    
     pass
